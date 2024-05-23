064a919d8a6f9c4045855560114cff1549e21fae
CDLemmi <c.lemmerich@gmx.de>
2024-05-23 02:00:24 +0200

    Added feature to autolaunch a game without having to create a new world trough the menu. Enable by having one command line argument

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using SeeloewenCraft;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+using static System.Environment;
 
 namespace Random_2D_Terrain_Generator_2._0
 {
@@ -13,5 +16,37 @@ namespace Random_2D_Terrain_Generator_2._0
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+
+            switch (e.Args.Length)
+            {
+                case 0:
+                    wndMenu wndMenu = new wndMenu();
+                    wndMenu.Show();
+                    break;
+                case 1:
+                    string appData = GetFolderPath(SpecialFolder.ApplicationData);
+
+                    if (!Directory.Exists(string.Format("{0}/SeeloewenCraft/", appData)))
+                    {
+                        Directory.CreateDirectory(string.Format("{0}/SeeloewenCraft/", appData));
+                    }
+                    string gameDirectory = string.Format("{0}/SeeloewenCraft/", appData);
+
+                    if (Directory.Exists(string.Format("{0}/{1}", gameDirectory, "debug")))
+                    {
+                        Directory.Delete(string.Format("{0}/{1}", gameDirectory, "debug"));
+                    }
+
+
+                    wndGame wndGame = new wndGame("debug", true, "currently unused");
+                    wndGame.Show();
+
+                    break;
+            }
+
+        }
     }
 }
