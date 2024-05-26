6a42e1563d25b14fe5478ef474bee876d8256426
CDLemmi <c.lemmerich@gmx.de>
2024-05-26 23:47:18 +0200

    fixed bug that world wasnt newly generated when auto launching a world without main menu

--- App.xaml.cs
+++ App.xaml.cs
@@ -35,9 +35,9 @@ namespace Random_2D_Terrain_Generator_2._0
                     }
                     string gameDirectory = string.Format("{0}/SeeloewenCraft/", appData);
 
-                    if (Directory.Exists(string.Format("{0}/{1}", gameDirectory, "debug")))
+                    if (Directory.Exists(string.Format("{0}/worlds/{1}", gameDirectory, "debug")))
                     {
-                        Directory.Delete(string.Format("{0}/{1}", gameDirectory, "debug"), true);
+                        Directory.Delete(string.Format("{0}/worlds/{1}", gameDirectory, "debug"), true);
                     }
 
                     wndGame wndGame = new wndGame(wndMenu, "debug", true, wndMenu.worldVersion, wndMenu.gameVersion);
